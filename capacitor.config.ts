e1bf3973c1c44fc68243a197a00eb253c994f190
Damian G <gilbertodamian14@gmail.com>
2024-03-06 22:51:45 -0600

    Practica 6: Inicializacion Angular e Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '2051492-IONIC-basico',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
